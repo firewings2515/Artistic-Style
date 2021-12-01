893585cda8859c5e22ac8e286b1e454be0fa1096
firewings2515 <capricorn9908@gmail.com>
2021-12-01 08:00:46 +0000

    update my result from colab

--- style.py
+++ style.py
@@ -10,7 +10,7 @@ MIT license
 
 import argparse
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import vgg
 from PIL import Image
 
@@ -97,56 +97,58 @@ WIDTH, HEIGHT = content_image.shape[1], content_image.shape[0]
 content_image = content_image.reshape((1, HEIGHT, WIDTH, 3))
 style_image = load_image(args.style_image_path, (WIDTH, HEIGHT))
 style_image = style_image.reshape((1, HEIGHT, WIDTH, 3))
+tf.executing_eagerly()
+with tf.Session() as sess:
+  # prepare networks
+  images = np.concatenate((content_image, style_image), 0).astype(np.float32)
+  constants = tf.constant(images)
+  with tf.name_scope("constant"):
+      vgg_const = vgg.Vgg19()
+      vgg_const.build(constants)
 
-# prepare networks
-images = np.concatenate((content_image, style_image), 0).astype(np.float32)
-constants = tf.constant(images)
-with tf.name_scope("constant"):
-    vgg_const = vgg.Vgg19()
-    vgg_const.build(constants)
+  # use noise as an initial image 
+  #input_image = tf.Variable(tf.truncated_normal([1, HEIGHT, WIDTH, 3], 0.5, 0.1))
+  # use content image as an initial image
+  input_image = tf.Variable(np.expand_dims(images[0,:,:,:], 0))
+  with tf.name_scope("variable"):
+      vgg_var = vgg.Vgg19()
+      vgg_var.build(input_image)
 
-# use noise as an initial image 
-#input_image = tf.Variable(tf.truncated_normal([1, HEIGHT, WIDTH, 3], 0.5, 0.1))
-# use content image as an initial image
-input_image = tf.Variable(np.expand_dims(images[0,:,:,:], 0))
-with tf.name_scope("variable"):
-    vgg_var = vgg.Vgg19()
-    vgg_var.build(input_image)
-
-# which layers we want to use?
-style_layers_const = [vgg_const.conv1_1, vgg_const.conv2_1, vgg_const.conv3_1, vgg_const.conv4_1, vgg_const.conv5_1]
-style_layers_var = [vgg_var.conv1_1, vgg_var.conv2_1, vgg_var.conv3_1, vgg_var.conv4_1, vgg_var.conv5_1]
-content_layer_const = vgg_const.conv4_2
-content_layer_var = vgg_var.conv4_2
-
-# get activations of content and style images as TF constants
-sess = tf.Session()
-layers = sess.run([content_layer_const] + style_layers_const)
-content_layer_const = tf.constant(np.expand_dims(layers[0][0,:,:,:], 0))
-style_layers_const = [tf.constant(np.expand_dims(layer[1,:,:,:], 0)) for layer in layers[1:]]
-
-# compose the loss function
-content_style_ratio = 1e-4
-loss_content = content_loss(content_layer_const, content_layer_var)
-loss_style = style_loss(style_layers_const, style_layers_var)
-style_weight = float(args.style_weight)
-overall_loss = (1 - style_weight) * content_style_ratio * loss_content + style_weight * loss_style
-
-# set up optimizer 
-output_image = tf.clip_by_value(tf.squeeze(input_image, [0]), 0, 1)
-train = tf.train.AdamOptimizer(learning_rate=0.05).minimize(overall_loss)
+  # which layers we want to use?
+  style_layers_const = [vgg_const.conv1_1, vgg_const.conv2_1, vgg_const.conv3_1, vgg_const.conv4_1, vgg_const.conv5_1]
+  style_layers_var = [vgg_var.conv1_1, vgg_var.conv2_1, vgg_var.conv3_1, vgg_var.conv4_1, vgg_var.conv5_1]
+  content_layer_const = vgg_const.conv4_2
+  content_layer_var = vgg_var.conv4_2
 
 
-# get the stylized image
-init = tf.initialize_all_variables()
-sess.run(init)
-min_loss, best_image = float("inf"), None
-for i in xrange(0,500):
-    image, loss, _ = sess.run([output_image, overall_loss, train])
-    if i % 5 == 0:
-        print('Iteration {}: {}'.format(i, loss))
-        if(loss < min_loss):
-            min_loss, best_image = loss, image
+
+  # get activations of content and style images as TF constants
+  layers = sess.run([content_layer_const] + style_layers_const)
+  content_layer_const = tf.constant(np.expand_dims(layers[0][0,:,:,:], 0))
+  style_layers_const = [tf.constant(np.expand_dims(layer[1,:,:,:], 0)) for layer in layers[1:]]
+
+  # compose the loss function
+  content_style_ratio = 1e-4
+  loss_content = content_loss(content_layer_const, content_layer_var)
+  loss_style = style_loss(style_layers_const, style_layers_var)
+  style_weight = float(args.style_weight)
+  overall_loss = (1 - style_weight) * content_style_ratio * loss_content + style_weight * loss_style
+
+  # set up optimizer 
+  output_image = tf.clip_by_value(tf.squeeze(input_image, [0]), 0, 1)
+  train = tf.train.AdamOptimizer(learning_rate=0.05).minimize(overall_loss)
+
+
+  # get the stylized image
+  init = tf.initialize_all_variables()
+  sess.run(init)
+  min_loss, best_image = float("inf"), None
+  for i in range(0,500):
+      image, loss, _ = sess.run([output_image, overall_loss, train])
+      if i % 5 == 0:
+          print('Iteration {}: {}'.format(i, loss))
+          if(loss < min_loss):
+              min_loss, best_image = loss, image
 
 # save the result
 best_image = np.clip(best_image, 0, 1)
